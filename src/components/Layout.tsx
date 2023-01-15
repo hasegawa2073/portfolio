d45939782c6d83206de87e58a356442d88c921ad
hasegawa2073 <hasegawatatsuya206@gmail.com>
2023-01-16 06:43:10 +0900

    feat: 背景のグラデーションとオブジェクト追加

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -2,13 +2,21 @@ import { ReactNode } from 'react';
 
 import Footer from './Footer';
 import Header from './Header';
+import styles from './Layout.module.scss';
 
 const Layout = ({ children }: { children: ReactNode }) => {
   return (
     <>
-      <Header />
-      <main>{children}</main>
-      <Footer />
+      <div className={styles.layout}>
+        <div className={styles.glass}>
+          <Header />
+          <main>{children}</main>
+          <Footer />
+        </div>
+        <div className={styles.circle_s}></div>
+        <div className={styles.circle_m}></div>
+        <div className={styles.circle_l}></div>
+      </div>
     </>
   );
 };
