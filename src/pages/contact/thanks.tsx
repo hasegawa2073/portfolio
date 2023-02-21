47f118d5f0d998900e331c0674d66886ef6bc6ee
hasegawa2073 <hasegawatatsuya206@gmail.com>
2023-02-22 00:26:03 +0900

    feat: お問い合わせ完了ページ作成

---
+++ src/pages/contact/thanks.tsx
@@ -0,0 +1,59 @@
+import Image from 'next/image';
+import Link from 'next/link';
+import { useRouter } from 'next/router';
+
+import Layout from '@/components/Layout';
+import SEO from '@/components/SEO';
+import styles from '@/styles/contact-thanks.module.scss';
+
+import { caveat, notoSansJP } from '../_app';
+
+const Thanks = () => {
+  const router = useRouter();
+
+  return (
+    <>
+      <SEO
+        pagePath={`${process.env.NEXT_PUBLIC_BASE_URL}${router.pathname}`}
+        pageTitle="お問い合わせありがとうございました"
+        pageDescription=""
+      />
+      <Layout>
+        <div className={notoSansJP.className}>
+          <section className={styles.section}>
+            <div className={styles.ttl_container}>
+              <h1 className={`${caveat.className} ${styles.main_ttl}`}>Thanks</h1>
+              <p className={styles.sub_ttl}>お問い合わせ完了</p>
+            </div>
+            <div className={styles.thanks}>
+              <Image
+                src="/my-icon.jpg"
+                width={200}
+                height={200}
+                alt="Tatsuya Hasegawaのアイコン"
+                className={styles.thanks__icon}
+              />
+              <p className={styles.thanks__message}>
+                お問い合わせありがとうございます。
+                <br />
+                ご入力いただいたメールアドレス宛に自動返信メールを配信しております。
+                <br />
+                完了メールが届かない場合、処理が正常に行われていない可能性があります。
+                <br />
+                大変お手数ですが、再度お問い合わせの手続きをお願い致します。
+              </p>
+              <p className={`${caveat.className} ${styles.thanks__name}`}>Tatsuya Hasegawa</p>
+              <p className={styles.thanks__linkContainer}>
+                <Link href="/" className={styles.thanks__link}>
+                  HOMEへ戻る
+                </Link>
+              </p>
+            </div>
+          </section>
+        </div>
+      </Layout>
+    </>
+  );
+};
+
+export default Thanks;
