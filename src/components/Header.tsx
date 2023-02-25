c823d7d487dd327fdbffa43d5dae4f1bc513374c
hasegawa2073 <hasegawatatsuya206@gmail.com>
2023-02-25 10:01:27 +0900

    feat: headerが少しDragできるようにした

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,5 +1,6 @@
 import { faBarsStaggered } from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { motion } from 'framer-motion';
 import { useContext, useEffect, useState } from 'react';
 
 import GlobalNavigation from './globalNavigation/GlobalNavigation';
@@ -17,24 +18,26 @@ const Header = () => {
     setIsFirstLoad(true);
   }, []);
   return (
-    <header className={styles.header}>
-      <nav className={styles.nav} role="navigation" aria-label="グローバルナビゲーション">
-        <GlobalNavigation />
-      </nav>
-      {isMenuOpen === false && isFirstLoad === false ? <MobileNavContainer /> : ''}
-      {isMenuOpen === false ? (
-        <button
-          role="button"
-          aria-pressed="false"
-          className={`${styles.button}`}
-          onClick={() => setIsMenuOpen((prev) => !prev)}
-        >
-          <FontAwesomeIcon icon={faBarsStaggered} />
-        </button>
-      ) : (
-        <MobileNavContainer />
-      )}
-    </header>
+    <motion.div drag dragConstraints={{ top: 0, bottom: 0, left: 0, right: 0 }} dragElastic={0.2}>
+      <header className={styles.header}>
+        <nav className={styles.nav} role="navigation" aria-label="グローバルナビゲーション">
+          <GlobalNavigation />
+        </nav>
+        {isMenuOpen === false && isFirstLoad === false ? <MobileNavContainer /> : ''}
+        {isMenuOpen === false ? (
+          <button
+            role="button"
+            aria-pressed="false"
+            className={`${styles.button}`}
+            onClick={() => setIsMenuOpen((prev) => !prev)}
+          >
+            <FontAwesomeIcon icon={faBarsStaggered} />
+          </button>
+        ) : (
+          <MobileNavContainer />
+        )}
+      </header>
+    </motion.div>
   );
 };
 
